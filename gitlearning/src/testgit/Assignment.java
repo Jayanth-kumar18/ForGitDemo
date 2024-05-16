a60cd01d479b7f2e435ba7632fa10cda9b56bc6f
jayanth-kumar18 <jayanth18jay@gmail.com>
2024-05-16 15:25:31 +0530

    Updated print method and added add method in TestChild branch

--- gitlearning/src/testgit/Assignment.java
+++ gitlearning/src/testgit/Assignment.java
@@ -2,6 +2,9 @@ package testgit;
 
 public class Assignment {
 	public String print() {
-     return "Returning assignment";
+     return "Returning assignment class in TestChild branch";
 }
+	public int add(int a,int b) {
+		return a+b;
+	}
 }
